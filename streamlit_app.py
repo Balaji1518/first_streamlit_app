754eeac659e615649371e54e4bfc95dc70148768
Balaji1518 <141138883+Balaji1518@users.noreply.github.com>
2023-08-01 16:43:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit
 streamlit.title('My Parents New Health Dinner');
+streamlit.header('Breakfast Menu');
+streamlit.text('Omega 3 & Blueberry Oatmeal');
+streamlit.text('kale, Spinach & Rocket Smootie');
+streamlit.text('Hard-Boiled Free-Range Egg');
